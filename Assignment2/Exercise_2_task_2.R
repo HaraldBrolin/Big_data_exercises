9433f11b2bafb91caf56f1935faf4f445b4eb25e
Rhedensjo <31846649+Rhedensjo@users.noreply.github.com>
2017-12-06 10:47:41 +0100

    problem with predict

--- Assignment2/Exercise_2_task_2.R
+++ Assignment2/Exercise_2_task_2.R
@@ -1,8 +1,12 @@
-library(readr)
-GeneExpressionData <- read_delim("~/Desktop/Big_data/Big_data_exercises/Assignment2/GeneExpressionData.txt", 
+library(readr); library(caret);library(glmnet); 
+#GeneExpressionData <- read_delim("~/Desktop/Big_data/Big_data_exercises/Assignment2/GeneExpressionData.txt", 
+#                                 "\t", escape_double = FALSE, trim_ws = TRUE)
+GeneExpressionData <- read_delim("GeneExpressionData.txt", 
                                  "\t", escape_double = FALSE, trim_ws = TRUE)
-MetaData <- read_delim("~/Desktop/Big_data/Big_data_exercises/Assignment2/MetaData.txt", 
-                                 "\t", escape_double = FALSE, trim_ws = TRUE)
+#MetaData <- read_delim("~/Desktop/Big_data/Big_data_exercises/Assignment2/MetaData.txt", 
+#                                 "\t", escape_double = FALSE, trim_ws = TRUE)
+MetaData <- read_delim("MetaData.txt", 
+                       "\t", escape_double = FALSE, trim_ws = TRUE)
 set.seed(2017)
 GeneExpressionData <- GeneExpressionData[,-1]
 #t_gene <- as.data.frame(t(GeneExpressionData))
@@ -12,15 +16,38 @@ GeneExpressionData <- GeneExpressionData[,-1]
 
 
 PC <- prcomp(t(GeneExpressionData))
-df_PC <-  as.data.frame(PC$x)
+df_PC <-  as.data.frame(PC$x[,1:20])
 
-df_PC <- cbind(MetaData$SubType, df_PC)
-names(df_PC)[1] <- "Subtype"
-df_PC$Subtype <- as.numeric(df_PC$Subtype == "IDHmut-codel")
+df_PC <- cbind(df_PC,MetaData$SubType)
+names(df_PC)[ncol(df_PC)] <- "Subtype"
+df_PC$Subtype <- as.numeric(df_PC$Subtype == "IDHmut-codel") #Codel = 1
 
 df_test <- sample(nrow(df_PC), nrow(df_PC) * 0.2) # Selscts 20% as test data
 df_train <- df_PC[-df_test,]
 df_test <- df_PC[df_test,]
+
+
+#--------------- Create the logistic regression
+for (k in 0:17){
+  temp_df_test <- df_train[(k*10)+(1:10),]
+  temp_df_train <- df_train[-((k*10)+(1:10)),]
+  log_model <-  glm(Subtype ~ temp_df_train$PC1, family = binomial("logit"), temp_df_train)
+  pre_log <- predict(log_model, as.data.frame(temp_df_test[,-21]), type = "response")
+  confusionMatrix(pre_res, data_test$type)
+  
+  
+}
+log_model <-  glm(Subtype ~ df_train$PC1, family = binomial("logit"), df_train)
+print(log_model)
+for (k in 3:21){
+  log_model <-  glm(Subtype ~ df_train[,2:k], family = binomial("logit"), df_train)
+  print(log_model)
+
+
+}
+
+
+
 
 #--------------- Create the logistic regression
 
@@ -34,7 +61,7 @@ pre_res <- predict(lg_model, as.matrix(df_test[,-1]), s = "lambda.min", type = "
 ans <- confusionMatrix(pre_res, df_test$Subtype)
 
 
-#----------------- Nu använder jag minst two komponenter
+#----------------- Nu anv?nder jag minst two komponenter
 for (k in 2:20){
   lg_model <- cv.glmnet(x = as.matrix((df_train[-1])[1:k]),
                         y = df_train$Subtype,
