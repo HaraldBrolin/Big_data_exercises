1ea15a4baa3dc43d031bc02a472a0af87c767331
Harald Brolin <haraldbrolintest@gmail.com>
2017-12-07 13:12:46 +0100

    Added some comments to perceptron

--- Assignment2/Exercise_2_task_3.R
+++ Assignment2/Exercise_2_task_3.R
@@ -3,35 +3,41 @@
 # Länk:
 # http://www.cs.ukzn.ac.za/~hughm/dm/content/slides07.pdf
 
+
+
 set.seed(201606)
 N <- 20
 x1 <- runif(N,-1,1)
 x2 <- runif(N,-1,1)
-X <- cbind(x1,x2)
-y <- ifelse(x2>x1,-1,1)
+x <- cbind(x1,x2)
+Y <- ifelse(x2>x1,-1,1)
+
+plot(x)
+points(subset(x,Y==1),col="black",pch="+",cex=2)
+points(subset(x,Y==-1),col="red",pch="-",cex=2)
 
 distance.from.plane = function(z,w,b) {
-  sum(z*w) + b
+  sum(z*w) + b # If False = 0, first iteration returns b (0), andra bara -1 b är -15
   }
-classify.linear = function(x,w,b) {
+classify.linear = function(x,w,b) {  # x, FALSE FALSE, 0
   distances = apply(x, 1, distance.from.plane, w, b)
-  return(ifelse(distances < 0, -1, +1))
+  return(ifelse(distances < 0, -1, +1)) #first only 1111
   }
 
-euclidean.norm = function(x) {sqrt(sum(x * x))}
+euclidean.norm = function(x) {sqrt(sum(x * x))} # multiply row-vector, the distance of the vector
 perceptron = function(x, y, learning.rate=1) {
-  w = vector(length = ncol(x)) # initialize w
+  w = vector(length = ncol(x)) # initialize w, with length = ncol, FALSE FALSE
   b = 0 # Initialize b
   k = 0 # count updates
-  R = max(apply(x, 1, euclidean.norm))
+  R = max(apply(x, 1, euclidean.norm)) # 1 refers to the rows, finds max distance 
   made.mistake = TRUE # to enter the while loop
   while (made.mistake) {
     made.mistake=FALSE # hopefully
-    yc <- classify.linear(x,w,b)
+    yc <- classify.linear(x,w,b) # predicts, first round 111111 50%
     for (i in 1:nrow(x)) {
-      if (y[i] != yc[i]) {
-        w <- w + learning.rate * y[i]*x[i,]
-        b <- b + learning.rate * y[i]*R^2
+      if (y[i] != yc[i]) { 
+        w <- w + learning.rate * y[i]*x[i,] #w ökar för varje fel prediction
+        b <- b + learning.rate * y[i]*R^2 # b ökar för varje fel, minskar r 2
         k <- k+1
         made.mistake=TRUE
         }
@@ -40,4 +46,8 @@ perceptron = function(x, y, learning.rate=1) {
   return(list(w=w/s,b=b/s,updates=k))
   }
 
-p <- perceptron(X, y)
+p <- perceptron(x, Y)
+intercept <- - p$b / p$w[[2]]
+
+slope <- - p$w[[1]] /p$ w[[2]]
+abline(intercept,slope,col="green")
