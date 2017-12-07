cdb994d60282e740c18b49c5bbf5b74373ae622b
Harald Brolin <bio13hbn@PASCAL.CS>
2017-12-07 16:50:19 +0100

    fixed some parts, have the two first plots

--- Assignment2/Exercise_2_task_1.R
+++ Assignment2/Exercise_2_task_1.R
@@ -4,16 +4,26 @@ library(glmnet)
 # the library for data and 'confusion matrix'
 library(MASS); library(kernlab); library(caret)
 
+set.seed(2017)
 
-#df_train <- read.csv("~/Documents/BD/Assignment2/BreastCancerDataTrain.txt", " ", escape_double = FALSE, trim_ws = TRUE)
-df_train <- read.csv("BreastCancerDataTrain.txt", header = TRUE, sep = " ")
-df_test <- read.csv("BreastCancerDataTest.txt", header = TRUE, sep = " ")
+BreastCancerDataTrain <- read.csv("BreastCancerDataTrain.txt", header = TRUE, sep = " ")
+#df_test <- read.csv("BreastCancerDataTest.txt", header = TRUE, sep = " ")
+
+
+df_test <- sample(nrow(BreastCancerDataTrain), nrow(BreastCancerDataTrain) * 0.2) # Selects 20% as test data
+df_train <- BreastCancerDataTrain[-df_test,]
+df_test <- BreastCancerDataTrain[df_test,]
 
 train_resp <- as.numeric(df_train$Diagnosis == "M") # Malignt = 1
 test_resp <- as.numeric(df_test$Diagnosis == "M")
 
-df_train_norm <- as.data.frame(scale(df_train[, -1]))
-df_test_norm <- as.data.frame(scale(df_test[, -1]))
+# For a better PCA we should scale the data
+#df_train_norm <- as.data.frame(scale(df_train[, -1]))
+#df_test_norm <- as.data.frame(scale(df_test[, -1]))
+
+#------------- Name is still norm, but nothing has been done
+df_train_norm <- as.data.frame(df_train[, -1])
+df_test_norm <- as.data.frame(df_test[, -1])
 
 df_train_norm <- cbind(train_resp, df_train_norm)
 names(df_train_norm)[1] <- "Diagnosis"
@@ -23,33 +33,43 @@ names(df_test_norm)[1] <- "Diagnosis"
 
 
 
-PC <- prcomp(t(df_train_norm))
-for (int in c(1,2,3,4,5,6,7,8,9,10,11)){
+PC <- prcomp(t(BreastCancerDataTrain[,-1])) # Nu kör vi på hela datasettet inte bara train
+for (int in c(1,2,3,4,5,6,7,8,9,10)){
   varName <- paste("PC_", int, sep = "")
   assign(varName, PC$x[,int])
 }
 
 df_pc <- data.frame(PC_1, PC_2, PC_3, PC_4, PC_5)
-pairs(df_pc) # Pairwise scatter plot for first 5 components
+pairs(df_pc, cex =1.2) # Pairwise scatter plot for first 5 components
+ggpairs(df_pc)
 
 # --------------------------------------
-# Task 2 # Testa Cross validations !!!!!! Efter normalisering så får vi 11 fel, innan får vi 7 st
+# Task 2 # Testa Cross validations !
 
 #df_test <- read_delim("~/Documents/BD/Assignment2/BreastCancerDataTest.txt", " ", escape_double = FALSE, trim_ws = TRUE)
-lda_model <- lda(df_train_norm$Diagnosis ~ ., df_train)
-lda_pred <- predict(lda_model, df_test_norm)
-table(lda_pred$class, df_test_norm$Diagnosis) # See the accuracy
+lda_model <- lda(Diagnosis ~ ., df_train)
+lda_pred <- predict(lda_model, df_test)
+#table(lda_pred$class, df_test$Diagnosis) # See the accuracy
+
+confusionMatrix(lda_pred$class, df_test$Diagnosis) 
 
 #---------------------------------------
 # Task 3 (blev inget med logit-funktionen) !!! Eventuellt fel
 # KÃ¶ra pÃ¥ hela datasettet eller pÃ¥ training sen test
 # First merge data test and train
 #df_all <- rbind(df_test, df_train)
-lr_model <- cv.glmnet(x = as.matrix(df_train[, -1]), 
-                      y = as.matrix(df_train_norm$Diagnosis), family = "binomial", 
-                      type.measure = "class", nfolds = 10)
+#lr_model <- cv.glmnet(x = as.matrix(df_train[, -1]), 
+#                      y = as.matrix(df_train_norm$Diagnosis), family = "binomial", 
+#                      type.measure = "class", nfolds = 10)
 
-pre_res <- predict(lr_model, as.matrix(df_test_norm[, -1]), s = "lambda.min", type = "class")
+# Kör crossvalidation. bättre med bara glmnet
+
+lr_model <- glmnet(x = as.matrix(df_train_norm[which(colnames(df_test) != "Diagnosis")]),
+                    y = as.matrix(df_train_norm$Diagnosis),
+                    family = "binomial") 
+
+
+pre_res <- predict(lr_model, as.matrix(df_test_norm[which(colnames(df_test)!="Diagnosis")]), type = "class") # Here is where i get error!!!!!!!!!!!!!!!!!!!!!!!!!
 confusionMatrix(pre_res, df_test_norm$Diagnosis)
 
 #--------------------------------------
