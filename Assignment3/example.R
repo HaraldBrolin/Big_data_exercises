b717151934c117ade519f798da4ba779e910a5f7
Harald Brolin <bio13hbn@SATYR.CS>
2017-12-13 09:32:27 +0100

    added models for ass 3

--- Assignment3/example.R
+++ Assignment3/example.R
@@ -11,6 +11,10 @@ data("Sonar")
 head(Sonar)
 pairs(Sonar[,10:15], col = Sonar$Class)
 
+Sonar <- sample(Sonar)
+Sonar <- sample(Sonar)
+Sonar <- sample(Sonar)
+
 set.seed(2017)
 id_train <- sample(1:dim(Sonar)[1], 0.8*dim(Sonar)[1])
 train_data <- Sonar[id_train, ]; rownames(train_data) <- 1:dim(train_data)[1]
@@ -23,9 +27,8 @@ ctrl <- trainControl(method = "repeatedcv",
                      number = 10,
                      classProbs = T)
 
-M_svm <- train(x = train_data[,-61],
-               y = train_data[,61],
-               method = "svmRadial",
+M_svm <- train(Class ~ ., train_data,
+               method = 'svmLinear',
                metric = "Accuracy", 
                trControl = ctrl)
 
@@ -34,19 +37,19 @@ M_svm <- train(x = train_data[,-61],
 # two training parameter
 
 grid <- expand.grid(sigma = c(0.01, 3), C = c(0.75, 0.9, 1, 1.1, 1.25))
-
+grid <- expand.grid(C = c(0.75, 0.9, 1, 1.1, 1.25))
 ctrl <- trainControl(method = "repeatedcv",
                      repeats = 1,
                      number = 10,
                      search = "random", 
                      classProbs = T)
 
-M_svm <- train(x = train_data[,-61],
-               y = train_data[,61],
-               method = "svmRadial",
+M_svm <- train(Class ~., train_data,
+               method = "svmLinear",
                tuneGrid = grid,
                trControl = ctrl)
 
 M_svm
 
-predict(M_svm, test_data)
+confusionMatrix(predict(M_svm, test_data), test_data$Class)
+
